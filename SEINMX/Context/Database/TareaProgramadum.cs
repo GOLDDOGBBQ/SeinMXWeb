77f25a86e532b01a3f11c1aec6217a0c10f5a5a7
GOLDDOGBBQ <102876897+GOLDDOGBBQ@users.noreply.github.com>
2026-01-03 19:50:27 -0600

    ADD ADMIN

---
+++ SEINMX/Context/Database/TareaProgramadum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEINMX.Context.Database;
+
+public partial class TareaProgramadum
+{
+    public int IdTareaProgramada { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string ExpresionCron { get; set; } = null!;
+
+    public DateTimeOffset? UltimaEjecucion { get; set; }
+
+    public bool Activa { get; set; }
+
+    public string ZonaHoraria { get; set; } = null!;
+
+    public string CreadoPor { get; set; } = null!;
+
+    public DateTime FchReg { get; set; }
+
+    public string UsrReg { get; set; } = null!;
+
+    public string? ModificadoPor { get; set; }
+
+    public DateTime? FchAct { get; set; }
+
+    public string? UsrAct { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public DateTimeOffset? FchActivacion { get; set; }
+}
---
+++ SEINMX/Context/Database/TipoCambioDof.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEINMX.Context.Database;
+
+public partial class TipoCambioDof
+{
+    public int IdTipoCambioDof { get; set; }
+
+    public decimal? TipoCambio { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public string CreadoPor { get; set; } = null!;
+
+    public DateTime FchReg { get; set; }
+
+    public string UsrReg { get; set; } = null!;
+
+    public string? ModificadoPor { get; set; }
+
+    public DateTime? FchAct { get; set; }
+
+    public string? UsrAct { get; set; }
+}
