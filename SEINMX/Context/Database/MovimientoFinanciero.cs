818119a27cae74fc40935195b8831a994be8b450
GOLDDOGBBQ <102876897+GOLDDOGBBQ@users.noreply.github.com>
2026-06-02 17:38:50 -0600

    add MovimientoFinanciero and VsOrdenCompraDetalle classes

---
+++ SEINMX/Context/Database/MovimientoFinanciero.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEINMX.Context.Database;
+
+public partial class MovimientoFinanciero
+{
+    public int IdMovimientoFinanciero { get; set; }
+
+    public byte Tipo { get; set; }
+
+    public DateOnly Fecha { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public decimal Monto { get; set; }
+
+    public int? IdProveedor { get; set; }
+
+    public string Factura { get; set; } = null!;
+
+    public bool PendienteFacturar { get; set; }
+
+    public int Orden { get; set; }
+
+    public string? CreadoPor { get; set; }
+
+    public DateTime FchReg { get; set; }
+
+    public string UsrReg { get; set; } = null!;
+
+    public string? ModificadoPor { get; set; }
+
+    public DateTime? FchAct { get; set; }
+
+    public string? UsrAct { get; set; }
+
+    public bool Eliminado { get; set; }
+
+    public virtual Proveedor? IdProveedorNavigation { get; set; }
+}
---
+++ SEINMX/Context/Database/VsOrdenCompraDetalle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEINMX.Context.Database;
+
+public partial class VsOrdenCompraDetalle
+{
+    public int IdOrdenCompraDetalle { get; set; }
+
+    public int IdOrdenCompra { get; set; }
+
+    public int IdCotizacionDetalle { get; set; }
+
+    public string Codigo { get; set; } = null!;
+
+    public string? CodigoProveedor { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public decimal PrecioListaMxn { get; set; }
+
+    public decimal PorcentajeProveedor { get; set; }
+
+    public decimal PorcentajeProveedorGanancia { get; set; }
+
+    public decimal PrecioProveedor { get; set; }
+
+    public decimal PrecioSein { get; set; }
+
+    public decimal Cantidad { get; set; }
+
+    public decimal Total { get; set; }
+}
